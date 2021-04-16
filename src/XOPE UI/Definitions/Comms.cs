3e39ac2c2ddd67bde5160c7070a0f815772836bb
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-04-17 00:53:33 +0100

    Moved project from / -> src/ | Added Spy packet types

---
+++ src/XOPE UI/Definitions/Comms.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Definitions
+{
+    namespace Comms
+    {
+
+    }
+
+    public enum ServerPacketType
+    {
+        CONNECTED_SUCCESS,
+        HOOKED_FUNCTION_CALL,
+        REQUEST_SOCKET_INFO,
+        PING
+    }
+
+    public enum SpyPacketType
+    {
+       INJECT_SEND,
+       INJECT_RECV,
+       SHUTDOWN_RECV_THREAD
+    }
+
+    public enum HookedFuncType
+    {
+        CONNECT,
+        SEND,
+        RECV,
+        CLOSE,
+        WSACONNECT,
+        WSASEND,
+        WSARECV
+    }
+
+    //Maybe pass a struct to the spy like this
+    /*
+     * json = {
+     *    type: 3
+     *    args: [
+     *      "test",
+     *      1,
+     *      2,
+     *      3
+     *    ]
+     * 
+     */
+    //public enum SpyPacketType
+    //{
+
+    //}
+}
