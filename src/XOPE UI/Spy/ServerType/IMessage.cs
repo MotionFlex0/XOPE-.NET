c50d5c6940587063d0d34d8e215b24a967a56dba
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-04-17 00:57:01 +0100

    Moved project from / -> src/ | Added some basic Spy message types

---
+++ src/XOPE UI/Spy/ServerType/IMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using PeterO.Cbor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOPE_UI.Definitions;
+
+namespace XOPE_UI.Spy
+{
+    public class IMessage
+    {
+        protected SpyPacketType Type { get; set; }
+        public string ToJson()
+        {
+            //CBORObject.FromObject(this).EncodeToBytes();
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ src/XOPE UI/Spy/ServerType/InjectSendPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.ServerType
+{
+    class InjectSendPacket : IMessage
+    {
+        public byte[] Data { get; set; }
+        public int SocketId { get; set; }
+
+        public InjectSendPacket()
+        {
+            Type = Definitions.SpyPacketType.INJECT_SEND;
+        }
+    }
+}
