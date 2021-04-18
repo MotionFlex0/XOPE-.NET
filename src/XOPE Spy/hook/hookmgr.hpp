59391117047fec61ea607e71601da959e44a1a49
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-04-18 01:03:54 +0100

    Removed all the now obsolete member functions/variables

--- src/XOPE Spy/hook/hookmgr.hpp
+++ src/XOPE Spy/hook/hookmgr.hpp
@@ -25,22 +25,9 @@ typedef int (WINAPI* WSARecvPtr_t)(SOCKET, LPWSABUF, DWORD, LPDWORD, LPDWORD, LP
 //CloseSocketPtr_t closes both 1.x and 2.x
 
 
-//TODO: Fix up this class. It is very messy
 class HookManager
 {
 public: 
-	struct HookedFuncArgs
-	{
-		void* connect = nullptr;
-		void* send = nullptr;
-		void* recv = nullptr;
-		void* close = nullptr;
-		void* wsaconnect = nullptr;
-		void* wsasend = nullptr;
-		void* wsarecv = nullptr;
-	};
-
-	//TODO: Possibly move HookFuncArgs here and keep "init" as some sort of "InitHooks" method
 	HookManager() { }
 
 	~HookManager()
@@ -48,49 +35,9 @@ public:
 		destroy();
 	}
 
-	void init(HookedFuncArgs hf) {
-		HMODULE hWs2 = GetModuleHandleA("ws2_32.dll");
-		if (hWs2 == NULL)
-		{
-			destroyed = true;
-			return;
-		}
-
-		if (hf.connect) connectdetour = new Detour(hf.connect, GetProcAddress(hWs2, "connect"), SENDPATCHSIZE);
-		if (hf.send) senddetour = new Detour(hf.send, GetProcAddress(hWs2, "send"), SENDPATCHSIZE);
-		if (hf.recv) recvdetour = new Detour(hf.recv, GetProcAddress(hWs2, "recv"), SENDPATCHSIZE);
-		if (hf.close) closedetour = new Detour(hf.close, GetProcAddress(hWs2, "closesocket"), CLOSEPATCHSIZE);
-		if (hf.wsaconnect) wsaconnectdetour = new Detour(hf.wsaconnect, GetProcAddress(hWs2, "WSAConnect"), SENDPATCHSIZE);
-		if (hf.wsasend) wsasenddetour = new Detour(hf.wsasend, GetProcAddress(hWs2, "WSASend"), SENDPATCHSIZE);
-		if (hf.wsarecv) wsarecvdetour = new Detour(hf.wsarecv, GetProcAddress(hWs2, "WSARecv"), SENDPATCHSIZE);
-
-		if (hf.connect) _oConnect = (ConnectPtr_t)connectdetour->patch();
-		if (hf.send) _oSend = (SendPtr_t)senddetour->patch();
-		if (hf.recv) _oRecv = (RecvPtr_t)recvdetour->patch();
-		if (hf.close) _oClose = (CloseSocketPtr_t)closedetour->patch();
-		if (hf.wsaconnect) _oWSAConnect = (WSAConnectPtr_t)wsaconnectdetour->patch();
-		if (hf.wsasend) _oWSASend = (WSASendPtr_t)wsasenddetour->patch();
-		if (hf.wsarecv) _oWSARecv = (WSARecvPtr_t)wsarecvdetour->patch();
-	}
-
 	void destroy() {
 		if (!destroyed)
 		{
-			if (connectdetour) connectdetour->unpatch();
-			if (senddetour) senddetour->unpatch();
-			if (recvdetour) recvdetour->unpatch();
-			if (closedetour) closedetour->unpatch();
-			if (wsaconnectdetour) wsaconnectdetour->unpatch();
-			if (wsasenddetour) wsasenddetour->unpatch();
-			if (wsarecvdetour) wsarecvdetour->unpatch();
-			if (connectdetour) delete connectdetour;
-			if (senddetour) delete senddetour;
-			if (recvdetour) delete recvdetour;
-			if (closedetour) delete closedetour;
-			if (wsaconnectdetour) delete wsaconnectdetour;
-			if (wsasenddetour) delete wsasenddetour;
-			if (wsarecvdetour) delete wsarecvdetour;
-
 			for (auto& [_, v] : _hooks)
 			{
 				v.detour->unpatch();
@@ -126,41 +73,6 @@ public:
 		return (std::any_cast<Func_t>(((*search).second).oFunc)(std::forward<Ts>(args)...));
 	}
 
-	template<typename... Ts>
-	const int oConnect(Ts&&... args) {
-		return _oConnect(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oSend(Ts&&... args) {
-		return _oSend(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oRecv(Ts&&... args) {
-		return _oRecv(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oClose(Ts&&... args) {
-		return _oClose(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oWSAConnect(Ts&&... args) {
-		return _oWSAConnect(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oWSASend(Ts&&... args) {
-		return _oWSASend(std::forward<Ts>(args)...);
-	}
-
-	template<typename... Ts>
-	const int oWSARecv(Ts&&... args) {
-		return _oWSARecv(std::forward<Ts>(args)...);
-	}
-
 private:
 
 	struct HookFuncData
@@ -172,21 +84,4 @@ private:
 	bool destroyed = false;
 
 	std::unordered_map<uintmax_t, HookFuncData> _hooks;
-
-	ConnectPtr_t _oConnect;
-	SendPtr_t _oSend;
-	RecvPtr_t _oRecv;
-	CloseSocketPtr_t _oClose;
-	WSAConnectPtr_t _oWSAConnect;
-	WSASendPtr_t _oWSASend;
-	WSARecvPtr_t _oWSARecv;
-
-	Detour* connectdetour = nullptr;
-	Detour* senddetour = nullptr;
-	Detour* recvdetour = nullptr;
-	Detour* closedetour = nullptr;
-	Detour* wsaconnectdetour = nullptr;
-	Detour* wsasenddetour = nullptr;
-	Detour* wsarecvdetour = nullptr;
-
 };
