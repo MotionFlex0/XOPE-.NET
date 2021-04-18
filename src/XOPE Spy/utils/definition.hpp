502f45257850aae4e38929ca4897f3ff6e568f9e
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-04-18 01:01:55 +0100

    Fixed typo

--- src/XOPE Spy/utils/definition.hpp
+++ src/XOPE Spy/utils/definition.hpp
@@ -1,11 +1,11 @@
 #if _WIN64
-using Deoour = Detour64;
+using Detour = Detour64;
 	#define MESSAGEBOXAPATCHSIZE 14
-	#define SENDPATCHSIZE 15
+	#define DEFAULTPATCHSIZE 15
     #define CLOSEPATCHSIZE 15
 #else
 using Detour = Detour32;
 	#define MESSAGEBOXAPATCHSIZE 5
-	#define SENDPATCHSIZE 5
+	#define DEFAULTPATCHSIZE 5
 	#define CLOSEPATCHSIZE 5
 #endif
