8f166086b07073756d0cead2a49063b4f4c755c2
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-05-22 01:47:10 +0100

    Added sample project for HexEditor component

---
+++ src/Samples/Samples.HexEditor/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samples.HexEditor
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.hexEditor.SetBytes(new byte[] { 0x01, 0x02, 0xFF });
+        }
+
+ 
+    }
+}
