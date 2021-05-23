724828822f97e8659c7e8620d947aca2361fff08
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-05-23 02:03:35 +0100

    Update Form1.Designer.cs

--- src/Samples/Samples.HexEditor/Form1.Designer.cs
+++ src/Samples/Samples.HexEditor/Form1.Designer.cs
@@ -39,14 +39,14 @@ namespace Samples.HexEditor
             | System.Windows.Forms.AnchorStyles.Right)));
             this.hexEditor.Location = new System.Drawing.Point(12, 26);
             this.hexEditor.Name = "hexEditor";
-            this.hexEditor.Size = new System.Drawing.Size(654, 302);
+            this.hexEditor.Size = new System.Drawing.Size(953, 367);
             this.hexEditor.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(686, 352);
+            this.ClientSize = new System.Drawing.Size(977, 417);
             this.Controls.Add(this.hexEditor);
             this.Name = "Form1";
             this.Text = "Form1";
