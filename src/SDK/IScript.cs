5315dab962108d02bc98f5ffcdeef194b1e9213c
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-06-05 00:01:09 +0100

    Added SDK project

---
+++ src/SDK/IScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDK
+{
+    public interface IScript
+    {
+        void Init();
+        //void Tick();
+    }
+}
