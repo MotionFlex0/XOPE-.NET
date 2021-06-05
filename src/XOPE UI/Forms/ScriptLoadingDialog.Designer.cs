ffe5b92a050e02c5c42fccdc7706b5ba93c0d58f
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-06-05 01:50:09 +0100

    Creating script loading dialog

---
+++ src/XOPE UI/Forms/ScriptLoadingDialog.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace XOPE_UI.Forms
+{
+    partial class ScriptLoadingDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusTextbox = new System.Windows.Forms.TextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(38, 38);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(122, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 0;
+            // 
+            // statusTextbox
+            // 
+            this.statusTextbox.Location = new System.Drawing.Point(38, 12);
+            this.statusTextbox.Name = "statusTextbox";
+            this.statusTextbox.ReadOnly = true;
+            this.statusTextbox.Size = new System.Drawing.Size(122, 20);
+            this.statusTextbox.TabIndex = 1;
+            this.statusTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(58, 67);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(82, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // ScriptLoadingDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 100);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.statusTextbox);
+            this.Controls.Add(this.progressBar);
+            this.Name = "ScriptLoadingDialog";
+            this.Text = "Loading Script";
+            this.Load += new System.EventHandler(this.ScriptLoadingDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox statusTextbox;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/XOPE UI/Forms/ScriptLoadingDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOPE_UI.Forms
+{
+    public partial class ScriptLoadingDialog : Form
+    {
+        CancellationTokenSource _cancellationTokenSource;
+        public ScriptLoadingDialog(CancellationTokenSource cancellationTokenSource)
+        {
+            InitializeComponent();
+            _cancellationTokenSource = cancellationTokenSource;
+        }
+
+        // Called when script has finished loading
+        public void ScriptLoaded()
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void ScriptLoadingDialog_Load(object sender, EventArgs e)
+        {
+            this.statusTextbox.Text = "Loading script...";
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
