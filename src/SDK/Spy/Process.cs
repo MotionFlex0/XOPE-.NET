c8cac11bda65b5bbb6f33560c3ccc0b49ad37d16
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-06-12 23:13:56 +0100

    Create Process.cs

---
+++ src/SDK/Spy/Process.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDK.Spy
+{
+    class Process
+    {
+        public static IntPtr GetProcessHandle()
+        {
+
+            return default;
+        }
+    }
+}
