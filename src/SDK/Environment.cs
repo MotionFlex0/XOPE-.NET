dc0eb0e79959477c90c8b754ceb9b252d63cf8ff
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-06-12 23:13:49 +0100

    Create Environment.cs

---
+++ src/SDK/Environment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOPE_UI.Spy;
+
+namespace SDK
+{
+    class Environment
+    {
+        public event EventHandler<IntPtr> OnProcessAttached;
+        public event EventHandler<IntPtr> OnProcessDetached;
+
+        public event EventHandler<byte[]> OnNewSendPacket;
+        public event EventHandler<byte[]> OnNewRecvPacket;
+        public event EventHandler<byte[]> OnNewSendPacketModify;
+        public event EventHandler<byte[]> OnNewRecvPacketModify;
+
+
+        public IntPtr Process { get; set; }
+        public IServer Server { get; set; }
+
+        private static Environment environment;
+
+        private Environment() { }
+
+        public static Environment GetEnvironment() => 
+            environment ?? (environment = new Environment());
+
+
+
+    }
+}
