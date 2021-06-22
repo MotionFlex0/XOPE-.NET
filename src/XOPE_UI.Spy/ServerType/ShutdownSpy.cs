3aab1971e2794f685c235be45701983e64a27638
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-06-22 23:54:02 +0100

    Create ShutdownSpy.cs

---
+++ src/XOPE_UI.Spy/ServerType/ShutdownSpy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.ServerType
+{
+    class ShutdownSpy : IMessage
+    {
+        public ShutdownSpy()
+        {
+            Type = Definitions.SpyMessageType.SHUTDOWN_RECV_THREAD;
+        }
+    }
+}
