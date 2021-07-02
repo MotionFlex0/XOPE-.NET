ab1fecf7a9297f931df67a42cdfcdf4a16a5ef30
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-07-02 23:53:43 +0100

    Fixed auto-updating for Log Dialog

--- src/XOPE UI/Util/Logger.cs
+++ src/XOPE UI/Util/Logger.cs
@@ -30,22 +30,188 @@ namespace XOPE_UI.Util
             //OnFlush?.Invoke(this, this.ToString());
         }
 
+
         public override Task FlushAsync()
         {
             return Task.Run(() => Flush());
         }
 
-        public void Write<T>(T value)
+        public override void Write(bool value)
         {
             base.Write(value);
             OnFlush?.Invoke(this, value.ToString());
         }
 
-        public void WriteLine<T>(T value)
+        public override void Write(char value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(char[] buffer)
+        {
+            base.Write(buffer);
+            OnFlush?.Invoke(this, new string(buffer));
+        }
+
+        public override void Write(char[] buffer, int index, int count)
+        {
+            base.Write(buffer, index, count);
+            OnFlush?.Invoke(this, new string(buffer, index, count));
+        }
+
+        public override void Write(decimal value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(double value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(float value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(int value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(long value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(object value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString() + "\r\n");
+        }
+
+        public override void Write(string value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value);
+        }
+
+        public override void Write(uint value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void Write(ulong value)
+        {
+            base.Write(value);
+            OnFlush?.Invoke(this, value.ToString());
+        }
+
+        public override void WriteLine(bool value)
         {
             base.WriteLine(value);
             OnFlush?.Invoke(this, value + "\r\n");
         }
+
+        public override void WriteLine(char value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(char[] buffer)
+        {
+            base.WriteLine(buffer);
+            OnFlush?.Invoke(this, new string(buffer) + "\r\n");
+        }
+
+        public override void WriteLine(char[] buffer, int index, int count)
+        {
+            base.WriteLine(buffer, index, count);
+            OnFlush?.Invoke(this, new string(buffer, index, count) + "\r\n");
+        }
+
+        public override void WriteLine(decimal value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(double value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(float value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(int value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(long value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(object value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(string format, object arg0)
+        {
+            base.WriteLine(format, arg0);
+            OnFlush?.Invoke(this, String.Format(format, arg0) + "\r\n");
+        }
+
+        public override void WriteLine(string value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(uint value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        public override void WriteLine(ulong value)
+        {
+            base.WriteLine(value);
+            OnFlush?.Invoke(this, value + "\r\n");
+        }
+
+        //public void Write<T>(T value)
+        //{
+        //    base.Write(value);
+        //    MessageBox.Show($"Write<T>(T value) with value type: {value.GetType().Name}");
+        //    OnFlush?.Invoke(this, value.ToString());
+        //}
+
+        //public void WriteLine<T>(T value)
+        //{
+        //    base.WriteLine(value);
+        //    MessageBox.Show($"WriteLine<T>(T value) with value type: {value.GetType().Name}");
+        //    OnFlush?.Invoke(this, value + "\r\n");
+        //}
+
 
         public override void WriteLine()
         {
