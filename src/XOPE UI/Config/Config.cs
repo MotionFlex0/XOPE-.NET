9772d0bdc7f59c002ab963c66f269bd9a62599e9
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-07-05 23:58:27 +0100

    Renamed config file

--- src/XOPE UI/Config/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace XOPE_UI.Config
-{
-    class Config
-    {
-        const string XOPE_SPY_32 = "XOPESpy32.dll";
-        const string XOPE_SPY_64 = "XOPESpy64.dll";
-    }
-}
---
+++ src/XOPE UI/Config/Spy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XOPE_UI.Config
+{
+    public class Spy
+    {
+        public static string ModuleName32 { get; } = "XOPESpy32.dll";
+        public static string ModuleName64 { get; } = "XOPESpy64.dll";
+
+        public static string ModulePath32 { get; } = $@"{Environment.CurrentDirectory}\{ModuleName32}";
+        public static string ModulePath64 { get; } = $@"{Environment.CurrentDirectory}\{ModuleName64}";
+
+    }
+}
