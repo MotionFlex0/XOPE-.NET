c78a8904e55072d7634b4ffc2c457b3fad637ee9
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-08-10 23:02:46 +0100

    Added Filter Editor

---
+++ src/XOPE UI/Forms/FilterEditorDialog.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace XOPE_UI.Forms
+{
+    partial class FilterEditorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.beforeHexEditor = new WpfHexaEditor.HexEditor();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.elementHost2 = new System.Windows.Forms.Integration.ElementHost();
+            this.hexEditor1 = new WpfHexaEditor.HexEditor();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.elementHost1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(818, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Before";
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(6, 15);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(806, 100);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.beforeHexEditor;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 152);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(818, 15);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "After";
+            // 
+            // elementHost2
+            // 
+            this.elementHost2.Location = new System.Drawing.Point(18, 171);
+            this.elementHost2.Name = "elementHost2";
+            this.elementHost2.Size = new System.Drawing.Size(806, 100);
+            this.elementHost2.TabIndex = 5;
+            this.elementHost2.Text = "elementHost2";
+            this.elementHost2.Child = this.hexEditor1;
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(690, 415);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(75, 23);
+            this.acceptButton.TabIndex = 3;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(771, 415);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // FilterEditorDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 450);
+            this.Controls.Add(this.elementHost2);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FilterEditorDialog";
+            this.Text = "Filter Dialog";
+            this.Load += new System.EventHandler(this.FilterEditorDialog_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private WpfHexaEditor.HexEditor beforeHexEditor;
+        private System.Windows.Forms.Integration.ElementHost elementHost2;
+        private WpfHexaEditor.HexEditor hexEditor1;
+    }
+}
---
+++ src/XOPE UI/Forms/FilterEditorDialog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XOPE_UI.Definitions;
+
+namespace XOPE_UI.Forms
+{
+    public partial class FilterEditorDialog : Form
+    {
+        //public bool EditMode { get; set; } = false;
+        //public FilterEntry Filter { get; set; }
+
+        public FilterEditorDialog()
+        {
+            InitializeComponent();
+
+            //beforeHexEditor.ForegroundSecondColor = System.Windows.Media.Brushes.Blue;
+            //beforeHexEditor.StatusBarVisibility = System.Windows.Visibility.Hidden;
+            //beforeHexEditor.StringByteWidth = 8;
+
+
+            //afterHexEditor.ForegroundSecondColor = System.Windows.Media.Brushes.Blue;
+            //afterHexEditor.StatusBarVisibility = System.Windows.Visibility.Hidden;
+            //afterHexEditor.StringByteWidth = 8;
+        }
+
+        private void FilterEditorDialog_Load(object sender, EventArgs e)
+        {
+            beforeHexEditor.Stream = new MemoryStream(new byte[] { 0x11, 0x22 });
+            hexEditor1.Stream = new MemoryStream(new byte[] { 0x55, 0x22 });
+
+            //if (Filter == null)
+            //    Filter = new FilterEntry();
+            //else
+            //{
+            //    //beforeHexEditor.SetBy(Filter.Before);
+            //    //afterHexEditor.SetBytes(Filter.After);
+            //}
+
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            //Filter = new FilterEntry
+            //{
+            //    Name = "test",
+            //    Before = beforeHexEditor.GetAllBytes(true),
+            //    After = afterHexEditor.GetAllBytes(true),
+            //    SocketId = 0
+            //};
+            //DialogResult = DialogResult.OK;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
