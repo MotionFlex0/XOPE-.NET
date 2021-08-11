51dec7542deb6023e6a84933b8ca3283f9161e40
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-08-11 18:29:21 +0100

    Create FilterEntry.cs

---
+++ src/XOPE_UI.Definitions/FilterEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Definitions
+{
+    public class FilterEntry
+    {
+        public string Name { get; set; }
+        public byte[] Before { get; set; }
+        public byte[] After { get; set; }
+        public int SocketId { get; set; }
+    }
+}
