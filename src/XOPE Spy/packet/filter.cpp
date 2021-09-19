e20a16dfdfef310817e1456acf7569cba24e5fd7
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-09-19 23:06:03 +0100

    Added findAndReplace function body

--- src/XOPE Spy/packet/filter.cpp
+++ src/XOPE Spy/packet/filter.cpp
@@ -35,13 +35,27 @@ bool FilterReplace::findAndReplace(Packet& packet)
 {
 	for (const auto& f : filterMap)
 	{
-		const Packet oldVal = f.second.oldVal;
+		const Packet& oldVal = f.second.oldVal;
 		if (oldVal.size() <= packet.size())
 		{
 			auto found = std::search(packet.begin(), packet.end(), oldVal.begin(), oldVal.end());
 			if (found != packet.end())
 			{
-				
+				const Packet& newVal = f.second.newVal;
+				const int delta = newVal.size() > oldVal.size();
+
+				std::copy(newVal.begin(), newVal.end() - delta, found);
+
+				if (delta > 0)
+				{
+					packet.insert(found + delta, newVal.begin() + delta, newVal.end());
+				}
+				else if (delta < 0)
+				{
+					auto endIt = found + oldVal.size();
+					packet.erase(endIt - delta - 1, endIt);
+				}
+				return true;
 			}
 		}
 		return false;
