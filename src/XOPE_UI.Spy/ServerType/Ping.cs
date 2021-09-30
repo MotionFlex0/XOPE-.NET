ded6a8ea3c3a5adc66b2a227add1eb91ce06c256
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-09-30 23:51:26 +0100

    Create Ping.cs

---
+++ src/XOPE_UI.Spy/ServerType/Ping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.ServerType
+{
+    public class Ping : IMessageWithResponse
+    {
+        public Ping()
+        {
+            Type = Definitions.SpyMessageType.PING;
+        }
+    }
+}
