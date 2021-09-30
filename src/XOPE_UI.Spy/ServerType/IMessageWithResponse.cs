6926355fd46eaa694b3ed05c194f146ee8560148
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-09-30 23:51:20 +0100

    Create IMessageWithResponse, allowing for async communication with the Spy

---
+++ src/XOPE_UI.Spy/ServerType/IMessageWithResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.ServerType
+{
+    public abstract class IMessageWithResponse : IMessage
+    {
+        public event EventHandler<JObject> OnResponse;
+
+        [JsonProperty]
+        public Guid JobId { get; } = Guid.NewGuid();
+
+        public void NotifyResponse(JObject resposne) =>
+            OnResponse?.Invoke(this, resposne);
+    }
+}
