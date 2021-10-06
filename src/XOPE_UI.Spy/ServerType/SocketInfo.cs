b2e39c3f129ce7a78341c2b39238c666b5af495f
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-10-06 23:49:34 +0100

    Create SocketInfo.cs

---
+++ src/XOPE_UI.Spy/ServerType/SocketInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.ServerType
+{
+    public class SocketInfo : IMessageWithResponse
+    {
+        public int SocketId { get; set; }
+
+        public SocketInfo()
+        {
+            Type = Definitions.SpyMessageType.REQUEST_SOCKET_INFO;
+        }
+    }
+}
