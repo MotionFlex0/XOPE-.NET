110fc09c3547343145c1c97614b36fa5f72c43f6
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-11-18 22:40:58 +0000

    Created EventArgs for events

---
+++ src/XOPE_UI.Spy/ConnectionEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XOPE_UI.Definitions;
+
+namespace XOPE_UI.Spy
+{
+    public class ConnectionEventArgs
+    {
+        public Connection Connection { get; set; }
+    }
+}
---
+++ src/XOPE_UI.Spy/PacketEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XOPE_UI.Spy
+{
+    public class PacketEventArgs : EventArgs
+    {
+        public Packet Packet { get; set; }
+    }
+}
