7d139ad49debebbc67d9c6f3690757830b1a636a
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-11-20 23:49:11 +0000

    Create IncomingMessage.cs

---
+++ src/XOPE_UI.Spy/Type/IncomingMessage.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOPE_UI.Definitions;
+
+namespace XOPE_UI.Spy.Type
+{
+    public record IncomingMessage
+    {
+        public ServerMessageType Type { get; }
+        public JObject Json { get; }
+
+        public IncomingMessage(ServerMessageType type, JObject json)
+        {
+            Type = type;
+            Json = json;
+        }
+    }
+}
