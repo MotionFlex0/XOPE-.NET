3f16ac584810aa8c4f03a1b5510f555dc6bb5c7b
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-11-22 23:39:27 +0000

    Fixed issues cause a crash due to a return value of -1

--- src/XOPE Spy/patches/recv.cpp
+++ src/XOPE Spy/patches/recv.cpp
@@ -9,12 +9,13 @@ int WINAPI Functions::Hooked_Recv(SOCKET s, char* buf, int len, int flags)
     client::HookedFunctionCallPacketMessage hfcm;
     hfcm.functionName = HookedFunction::RECV;
     hfcm.socket = s;
-    hfcm.packetDataB64 = client::IMessage::convertBytesToB64String(buf, bytesRead); //base64_encode(buf, len);
     hfcm.packetLen = bytesRead;
     hfcm.ret = bytesRead;
 
     if (bytesRead == SOCKET_ERROR)
         hfcm.lastError = WSAGetLastError();
+    else if (bytesRead > 0)
+        hfcm.packetDataB64 = client::IMessage::convertBytesToB64String(buf, bytesRead);
 
     app.sendToUI(hfcm);
 
