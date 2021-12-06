6e7040b0443b1c375f966902d3d73b923e0e052f
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-12-06 23:16:07 +0000

    Added Dialog allowing the user to choose a socket/connection from a list of established connections

---
+++ src/XOPE UI/Forms/SocketSelectorDialog.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace XOPE_UI.Forms
+{
+    partial class SocketSelectorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.connectionListView = new System.Windows.Forms.ListView();
+            this.socket = new System.Windows.Forms.ColumnHeader();
+            this.ipFamily = new System.Windows.Forms.ColumnHeader();
+            this.ip = new System.Windows.Forms.ColumnHeader();
+            this.port = new System.Windows.Forms.ColumnHeader();
+            this.status = new System.Windows.Forms.ColumnHeader();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.refreshButton.Location = new System.Drawing.Point(13, 154);
+            this.refreshButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(88, 27);
+            this.refreshButton.TabIndex = 5;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // connectionListView
+            // 
+            this.connectionListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectionListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.socket,
+            this.ipFamily,
+            this.ip,
+            this.port,
+            this.status});
+            this.connectionListView.FullRowSelect = true;
+            this.connectionListView.Location = new System.Drawing.Point(13, 12);
+            this.connectionListView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.connectionListView.MultiSelect = false;
+            this.connectionListView.Name = "connectionListView";
+            this.connectionListView.Size = new System.Drawing.Size(381, 136);
+            this.connectionListView.TabIndex = 4;
+            this.connectionListView.UseCompatibleStateImageBehavior = false;
+            this.connectionListView.View = System.Windows.Forms.View.Details;
+            this.connectionListView.SelectedIndexChanged += new System.EventHandler(this.connectionListView_SelectedIndexChanged);
+            this.connectionListView.DoubleClick += new System.EventHandler(this.connectionListView_DoubleClick);
+            // 
+            // socket
+            // 
+            this.socket.Text = "Socket ID";
+            // 
+            // ipFamily
+            // 
+            this.ipFamily.Text = "IP Family";
+            // 
+            // ip
+            // 
+            this.ip.Text = "IP Address";
+            this.ip.Width = 67;
+            // 
+            // port
+            // 
+            this.port.Text = "Port";
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            this.status.Width = 120;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.Location = new System.Drawing.Point(306, 154);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(88, 27);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.confirmButton.Enabled = false;
+            this.confirmButton.Location = new System.Drawing.Point(210, 154);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(88, 27);
+            this.confirmButton.TabIndex = 6;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // SocketSelectorDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 193);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.connectionListView);
+            this.Controls.Add(this.cancelButton);
+            this.Name = "SocketSelectorDialog";
+            this.Text = "Socket Selector";
+            this.Load += new System.EventHandler(this.SocketSelectorDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ListView connectionListView;
+        private System.Windows.Forms.ColumnHeader socket;
+        private System.Windows.Forms.ColumnHeader ipFamily;
+        private System.Windows.Forms.ColumnHeader ip;
+        private System.Windows.Forms.ColumnHeader port;
+        private System.Windows.Forms.ColumnHeader status;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button confirmButton;
+    }
+}
