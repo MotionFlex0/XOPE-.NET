cca8a99fa87b7c5055e5b9efdfbfd7bab8e20a86
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-12-23 23:54:45 +0000

    Updated ScriptManagerDialog

--- src/XOPE UI/Forms/ScriptManagerDialog.Designer.cs
+++ src/XOPE UI/Forms/ScriptManagerDialog.Designer.cs
@@ -29,23 +29,23 @@ namespace XOPE_UI.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("Name");
-            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("Status");
-            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("Running Time");
-            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("Started At");
-            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("File Location");
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Name");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Status");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Running Time");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Started At");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("File Location");
             this.runningScriptListView = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.stopButton = new System.Windows.Forms.Button();
             this.startButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.stoppedScriptListView = new System.Windows.Forms.ListView();
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.scriptInfoListView = new System.Windows.Forms.ListView();
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -55,10 +55,10 @@ namespace XOPE_UI.Forms
             // 
             this.runningScriptListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
-            this.runningScriptListView.HideSelection = false;
-            this.runningScriptListView.Location = new System.Drawing.Point(6, 19);
+            this.runningScriptListView.Location = new System.Drawing.Point(7, 22);
+            this.runningScriptListView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.runningScriptListView.Name = "runningScriptListView";
-            this.runningScriptListView.Size = new System.Drawing.Size(289, 183);
+            this.runningScriptListView.Size = new System.Drawing.Size(336, 211);
             this.runningScriptListView.TabIndex = 0;
             this.runningScriptListView.UseCompatibleStateImageBehavior = false;
             this.runningScriptListView.View = System.Windows.Forms.View.Details;
@@ -71,9 +71,10 @@ namespace XOPE_UI.Forms
             // 
             // stopButton
             // 
-            this.stopButton.Location = new System.Drawing.Point(533, 118);
+            this.stopButton.Location = new System.Drawing.Point(622, 136);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.Size = new System.Drawing.Size(88, 27);
             this.stopButton.TabIndex = 1;
             this.stopButton.Text = "Stop";
             this.stopButton.UseVisualStyleBackColor = true;
@@ -81,9 +82,11 @@ namespace XOPE_UI.Forms
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(384, 118);
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(448, 136);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.Size = new System.Drawing.Size(88, 27);
             this.startButton.TabIndex = 3;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
@@ -92,9 +95,11 @@ namespace XOPE_UI.Forms
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.runningScriptListView);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(303, 208);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(354, 240);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Running Scripts";
@@ -102,9 +107,11 @@ namespace XOPE_UI.Forms
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.stoppedScriptListView);
-            this.groupBox2.Location = new System.Drawing.Point(12, 240);
+            this.groupBox2.Location = new System.Drawing.Point(14, 277);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(303, 208);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Size = new System.Drawing.Size(354, 240);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Stopped Scripts";
@@ -113,10 +120,10 @@ namespace XOPE_UI.Forms
             // 
             this.stoppedScriptListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2});
-            this.stoppedScriptListView.HideSelection = false;
-            this.stoppedScriptListView.Location = new System.Drawing.Point(6, 19);
+            this.stoppedScriptListView.Location = new System.Drawing.Point(7, 22);
+            this.stoppedScriptListView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.stoppedScriptListView.Name = "stoppedScriptListView";
-            this.stoppedScriptListView.Size = new System.Drawing.Size(289, 183);
+            this.stoppedScriptListView.Size = new System.Drawing.Size(336, 211);
             this.stoppedScriptListView.TabIndex = 0;
             this.stoppedScriptListView.UseCompatibleStateImageBehavior = false;
             this.stoppedScriptListView.View = System.Windows.Forms.View.Details;
@@ -130,9 +137,11 @@ namespace XOPE_UI.Forms
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.scriptInfoListView);
-            this.groupBox3.Location = new System.Drawing.Point(321, 147);
+            this.groupBox3.Location = new System.Drawing.Point(374, 170);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(346, 197);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox3.Size = new System.Drawing.Size(404, 227);
             this.groupBox3.TabIndex = 7;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Script Information";
@@ -142,16 +151,16 @@ namespace XOPE_UI.Forms
             this.scriptInfoListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader3,
             this.columnHeader4});
-            this.scriptInfoListView.HideSelection = false;
             this.scriptInfoListView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem11,
-            listViewItem12,
-            listViewItem13,
-            listViewItem14,
-            listViewItem15});
-            this.scriptInfoListView.Location = new System.Drawing.Point(6, 19);
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5});
+            this.scriptInfoListView.Location = new System.Drawing.Point(7, 22);
+            this.scriptInfoListView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.scriptInfoListView.Name = "scriptInfoListView";
-            this.scriptInfoListView.Size = new System.Drawing.Size(332, 172);
+            this.scriptInfoListView.Size = new System.Drawing.Size(387, 198);
             this.scriptInfoListView.TabIndex = 0;
             this.scriptInfoListView.UseCompatibleStateImageBehavior = false;
             this.scriptInfoListView.View = System.Windows.Forms.View.Details;
@@ -168,14 +177,15 @@ namespace XOPE_UI.Forms
             // 
             // ScriptManagerDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 473);
+            this.ClientSize = new System.Drawing.Size(792, 546);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.stopButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "ScriptManagerDialog";
             this.Text = "ScriptManagerDialog";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ScriptManagerDialog_FormClosed);
