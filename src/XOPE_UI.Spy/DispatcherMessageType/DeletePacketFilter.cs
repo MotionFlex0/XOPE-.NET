414564fb9a9d6ae2758d401dc9944f8ed412ce59
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2021-12-28 23:53:36 +0000

    Renamed AddSendFilter and added DeletePacketFilter

---
+++ src/XOPE_UI.Spy/DispatcherMessageType/DeletePacketFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOPE_UI.Definitions;
+using XOPE_UI.Spy.Type;
+
+namespace XOPE_UI.Spy.DispatcherMessageType
+{
+    public class DeletePacketFilter : IMessageWithResponse
+    {
+        public string FilterId { get; set; }
+
+        public DeletePacketFilter()
+        {
+            Type = SpyMessageType.DELETE_PACKET_FILTER;
+        }
+
+        public DeletePacketFilter(EventHandler<IncomingMessage> callback) : this()
+        {
+            OnResponse += callback;
+        }
+    }
+}
