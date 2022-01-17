9c8f914799742b2f83e0946a9d54d3dc2d531b32
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-01-17 23:24:57 +0000

    Implemented FilterEditorDialog using MVP

---
+++ src/XOPE UI/Presenter/FilterEditorDialogPresenter.cs
@@ -0,0 +1,39 @@
+﻿using XOPE_UI.Model;
+using XOPE_UI.View;
+
+namespace XOPE_UI.Presenter
+{
+    public class FilterEditorDialogPresenter
+    {
+        IFilterEditorDialog _view;
+
+        public FilterEditorDialogPresenter(IFilterEditorDialog view)
+        {
+            _view = view;
+        }
+
+        public void SetFilter(FilterEntry filter)
+        {
+            _view.FilterName = filter.Name;
+            _view.OldValue = filter.OldValue;
+            _view.NewValue = filter.NewValue;
+            _view.SocketId = filter.SocketId;
+            _view.PacketType = filter.PacketType;
+            _view.ShouldRecursiveReplace = filter.RecursiveReplace;
+            _view.AllSockets = filter.SocketId == -1;
+        }
+
+        public void SaveFilter()
+        {
+            if (_view.Filter == null)
+                _view.Filter = new FilterEntry();
+
+            _view.Filter.Name = _view.FilterName;
+            _view.Filter.OldValue = _view.OldValue;
+            _view.Filter.NewValue = _view.NewValue;
+            _view.Filter.SocketId = _view.SocketId;
+            _view.Filter.PacketType = _view.PacketType;
+            _view.Filter.RecursiveReplace = _view.ShouldRecursiveReplace;
+        }
+    }
+}
---
+++ src/XOPE UI/View/IFilterEditorDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOPE_UI.Model;
+
+namespace XOPE_UI.View
+{
+    public interface IFilterEditorDialog
+    {
+        string FilterName { get; set; }
+        byte[] OldValue { get; set; }
+        byte[] NewValue { get; set; }
+        int SocketId { get; set; }
+        ReplayableFunction PacketType { get; set; }
+        bool ShouldRecursiveReplace { get; set; }
+        bool AllSockets { get; set; }
+        FilterEntry Filter { get; set; }
+    }
+}
