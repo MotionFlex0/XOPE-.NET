cd5256cf106a5fc89cd73738a02dfed2e7e25856
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-01-19 22:26:00 +0000

    Added loading dialog when attaching to a process

---
+++ src/XOPE UI/Forms/ProcessAttachingDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace XOPE_UI.View
+{
+    public partial class ProcessAttachingDialog : Form
+    {
+        string _processName;
+
+        public CancellationTokenSource CancellationToken { get; set; } = new();
+
+        public ProcessAttachingDialog(string processName)
+        {
+            InitializeComponent();
+            _processName = processName;
+        }
+
+        // Called when script has finished loading
+        public void CloseDialog()
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void ScriptLoadingDialog_Load(object sender, EventArgs e)
+        {
+            this.statusTextbox.Text = $"Attaching to {_processName}.exe...";
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            CancellationToken.Cancel();
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
