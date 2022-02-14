da736f90c1d389564098d18ee6f348ffdc4f14fe
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-02-14 23:52:35 +0000

    Added partial specialization for __stdcall calling convention under x86

--- src/XOPE Spy/hook/hookmgr.hpp
+++ src/XOPE Spy/hook/hookmgr.hpp
@@ -13,14 +13,13 @@ struct IHookedFuncWrapper;
 
 template <class F>
 struct OriginalFuncWrapper;
-template <class F>
-struct HookedFuncWrapper;
-
 
 template <class R, class... Args>
-struct OriginalFuncWrapper<R(*)(Args...)>
+struct OriginalFuncWrapper<R(__cdecl *)(Args...)>
 {
-	OriginalFuncWrapper(R(*lf)(Args...))
+	using Type = R(__cdecl*)(Args...);
+	
+	OriginalFuncWrapper(Type lf)
 	{
 		_function = lf;
 		
@@ -31,26 +30,51 @@ struct OriginalFuncWrapper<R(*)(Args...)>
 		return _function(args...);
 	}
 private:
-	R(*_function)(Args...);
+	Type _function;
 };
+
+#ifndef _WIN64
+template <class R, class... Args>
+struct OriginalFuncWrapper<R(__stdcall *)(Args...)>
+{
+	using Type = R(__stdcall*)(Args...);
+
+	OriginalFuncWrapper(Type lf)
+	{
+		_function = lf;
+	}
+
+	R __stdcall operator()(Args... args)
+	{
+		return _function(args...);
+	}
+private:
+	Type _function;
+};
+#endif
 
 struct IHookedFuncWrapper
 {
 	virtual int getReferenceCount() = 0;
 };
 
+template <class F>
+struct HookedFuncWrapper;
+
 template <class R, class... Args>
-struct HookedFuncWrapper<R(*)(Args...)> : public IHookedFuncWrapper
+struct HookedFuncWrapper<R(__cdecl *)(Args...)> : public IHookedFuncWrapper
 {
-	static HookedFuncWrapper<R(*)(Args...)>* getInstance()
+	using Type = R(__cdecl *)(Args...);
+
+	static HookedFuncWrapper<Type>* getInstance()
 	{
-		static HookedFuncWrapper<R(*)(Args...)> instance;
+		static HookedFuncWrapper<Type> instance;
 		return &instance;
 	}
 
 	static R invoke(Args... args)
 	{
-		HookedFuncWrapper<R(*)(Args...)>* this_ = getInstance();
+		HookedFuncWrapper<Type>* this_ = getInstance();
 		this_->increaseRefCount();
 		R ret = _function(args...);
 		this_->decreaseRefCount();
@@ -72,16 +96,67 @@ struct HookedFuncWrapper<R(*)(Args...)> : public IHookedFuncWrapper
 		_callRefCount++;
 	}
 
-	static R(*_function)(Args...);
+	static Type _function;
 private:
 	HookedFuncWrapper() { }
 
 	std::atomic_int _callRefCount = 0;
 };
 
+#ifndef _WIN64
+// TODO: Add a more permanant fix for this instead of 2 specialisation with 
+//		with different calling conventions
+template <class R, class... Args>
+struct HookedFuncWrapper<R(__stdcall*)(Args...)> : public IHookedFuncWrapper
+{
+	using Type = R(__stdcall *)(Args...);
+
+	static HookedFuncWrapper<Type>* getInstance()
+	{
+		static HookedFuncWrapper<Type> instance;
+		return &instance;
+	}
+
+	static R invoke(Args... args)
+	{
+		HookedFuncWrapper<Type>* this_ = getInstance();
+		this_->increaseRefCount();
+		R ret = _function(args...);
+		this_->decreaseRefCount();
+		return ret;
+	}
+
+	int getReferenceCount() override
+	{
+		return _callRefCount;
+	}
+
+	void decreaseRefCount()
+	{
+		_callRefCount--;
+	}
+
+	void increaseRefCount()
+	{
+		_callRefCount++;
+	}
+
+	static Type _function;
+private:
+	HookedFuncWrapper() { }
+
+	std::atomic_int _callRefCount = 0;
+};
+#endif
+
 // Definition for static class variable HookedFuncWrapper<..>::_function
 template <class R, class... Args>
-R(*HookedFuncWrapper<R(*)(Args...)>::_function)(Args...) = nullptr;
+R(__cdecl *HookedFuncWrapper<R(__cdecl *)(Args...)>::_function)(Args...) = nullptr;
+
+#ifndef _WIN64
+template <class R, class... Args>
+R(__stdcall *HookedFuncWrapper<R(__stdcall *)(Args...)>::_function)(Args...) = nullptr;
+#endif
 
 class HookManager
 {
@@ -146,19 +221,20 @@ public:
 		auto func = _F;
 		using _F_t = decltype(func);
 
-		auto search = m_hooks.find((uintptr_t)_F);
+		auto search = m_hooks.find((uintptr_t)func);
 
 		if (search == m_hooks.end())
 		{
 			char msg[1024];
 			sprintf_s(msg, sizeof(msg), "Could not find hook for this function - Calling _F\nFunction Address: %p\nFunction Type: %s", _F, typeid(_F).name());
 			MessageBoxA(NULL, msg, "Hook Missing - get_ofunction", MB_OK);
-			return OriginalFuncWrapper<_F_t>(_F);
+			return OriginalFuncWrapper<_F_t>(func);
 		}
 
 		std::stringstream ss;
 		ss << "call_ofunction _F did not match type of stored original function\n";
-		ss << "\n_F_t type: " << typeid(_F_t).name() << "\n\nsearch->second.oFunc type: " << search->second.oFunc.type().name();
+		ss << "\n_F_t type: " << typeid(_F_t).name() << "\n\nsearch->second.oFunc type: " << 
+			search->second.oFunc.type().name();
 		x_assert(typeid(_F_t) == search->second.oFunc.type(), ss.str().c_str());
 		
 		auto oFunc = std::any_cast<_F_t>(search->second.oFunc);
@@ -188,8 +264,6 @@ public:
 
 		auto oFunc = std::any_cast<_F_t>(search->second.oFunc);
 		return OriginalFuncWrapper<_F_t>(oFunc)(std::forward<Ts>(args)...);
-
-		/*return (std::any_cast<_F_t>(((*search).second).oFunc)(std::forward<Ts>(args)...))*/;
 	}
 
 private:
