d254962ec5818023ea8b75572dd4ab9970dcd3a2
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-03-06 23:39:18 +0000

    Created ControlExtensions

---
+++ src/XOPE_UI.Extensions/ControlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using XOPE_UI.Native;
+
+namespace XOPE_UI.Extensions
+{
+    //Credits: https://stackoverflow.com/questions/778095/windows-forms-using-backgroundimage-slows-down-drawing-of-the-forms-controls
+    public static class ControlExtensions
+    {
+        public static void ResumeDrawing(this Control control) =>
+            control.ResumeDrawing(true);
+
+        public static void ResumeDrawing(this Control control, bool shouldRedraw)
+        {
+            NativeMethods.SendMessage(control.Handle, Win32API.WindowsMessage.WM_SETREDRAW, 1, 0);
+            if (shouldRedraw)
+                control.Refresh();
+        }
+
+        public static void SuspendDrawing(this Control control) =>
+            NativeMethods.SendMessage(control.Handle, Win32API.WindowsMessage.WM_SETREDRAW, 0, 0);
+    }
+}
