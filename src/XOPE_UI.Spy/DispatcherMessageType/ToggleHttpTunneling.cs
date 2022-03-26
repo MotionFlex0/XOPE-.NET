b274efb7db6de9e0e106c41c7f5885973abeb6e0
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-03-26 23:24:32 +0000

    Added ToggleHttpTunnel message

---
+++ src/XOPE_UI.Spy/DispatcherMessageType/ToggleHttpTunneling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.DispatcherMessageType
+{
+    public class ToggleHttpTunneling : IMessage
+    {
+        public bool IsTunnelingEnabled { get; set; }
+
+        public ToggleHttpTunneling()
+        {
+            Type = Model.SpyMessageType.TOGGLE_HTTP_TUNNELING;
+        }
+    }
+}
