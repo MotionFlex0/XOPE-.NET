f3db9dbb9da385a5177205cbae114c59290c2a4d
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-03-30 23:52:18 +0100

    Create CloseSocketGracefully.cs

---
+++ src/XOPE_UI.Spy/DispatcherMessageType/CloseSocketGracefully.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOPE_UI.Spy.DispatcherMessageType
+{
+    public class CloseSocketGracefully : IMessage
+    {
+        public int SocketId { get; set; }
+
+        public CloseSocketGracefully()
+        {
+            Type = Model.SpyMessageType.CLOSE_SOCKET_GRACEFULLY;
+        }
+    }
+}
