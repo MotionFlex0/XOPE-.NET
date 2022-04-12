8d0e640c7b580bcdab4705d528d23f2fd065dcbd
Nathaniel <31831208+MotionFlex0@users.noreply.github.com>
2022-04-12 21:01:04 +0100

    Create socketdata.h

---
+++ src/XOPE Spy/definitions/socketdata.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <queue>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include "../packet/type.h"
+
+struct SocketData
+{
+	bool closeSocketGracefully = false;
+	bool isBlocking = false;
+	bool isTunneled = false;
+	std::queue<Packet> recvPacketsToInject;
+	int ipVersion = AF_INET;
+	bool socketIdSentToSink = true;
+};
