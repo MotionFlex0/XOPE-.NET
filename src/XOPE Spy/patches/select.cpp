42af679b3042981dc19db346ebbcad41ba8f9aa4
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2023-01-17 23:58:52 +0000

    select will now return a readable socket if there are packets to inject

--- src/XOPE Spy/patches/select.cpp
+++ src/XOPE Spy/patches/select.cpp
@@ -1,29 +1,39 @@
 #include "functions.h"
 #include "../application.h"
 
-
+// What this function does:
+//	- It will spoof a socket's readibility if there are packets that need to be injected
 int WSAAPI Functions::Hooked_Select(int nfds, fd_set FAR * readfds, fd_set FAR * writefds, fd_set FAR * exceptfds, const struct timeval FAR * timeout)
 {
 	Application& app = Application::getInstance();
 	
-	//std::vector<SOCKET> socketsToSpoof;
-	//
-	//if (writefds != nullptr)
-	//{
-	//	for (int i = 0; i < writefds->fd_count; i++)
-	//		if (app.isSocketTunneled(writefds->fd_array[i]))
-	//			socketsToSpoof.push_back(writefds->fd_array[i]);
-	//}
+	std::vector<SOCKET> socketsToSpoof;
 
-	int ret = app.getHookManager()->get_ofunction<select>()(nfds, readfds, writefds, exceptfds, timeout);
+	fd_set newReadfds;
+	FD_ZERO(&newReadfds);
+
+	int ret = 0;
+	if (readfds != nullptr)
+	{
+		for (int i = 0; i < readfds->fd_count; i++)
+		{
+			if (app.recvPacketsToInjectCount(readfds->fd_array[i]) > 0)
+				socketsToSpoof.push_back(readfds->fd_array[i]);
+			else
+				FD_SET(readfds->fd_array[i], &newReadfds);
+		}
+	}
 	
-	//if (writefds != nullptr && ret != SOCKET_ERROR)
-	//{
-	//	for (SOCKET s : socketsToSpoof)
-	//	{
-	//		FD_SET(s, writefds);
-	//		ret++;
-	//	}
-	//}
+	ret = app.getHookManager()->get_ofunction<select>()(nfds, &newReadfds, writefds, exceptfds, timeout);
+
+	if (readfds != nullptr)
+	{
+		for (SOCKET s : socketsToSpoof)
+		{
+			FD_SET(s, readfds);
+			ret++;
+		}
+	}
+
 	return ret;
 }
