d0a5a830e06ec72cb895c8c125ddbb23cfc88460
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2023-02-10 23:09:59 +0000

    Updated defintions.h and namespace for IMessage

--- src/XOPE Spy/utils/util.h
+++ src/XOPE Spy/utils/util.h
@@ -1,15 +1,16 @@
 #pragma once
 
-#include "../definitions/definitions.hpp"
 #include <string>
 #include <Windows.h>
 #include <source_location>
+
+#include "../dispatcher/type/impl/imessage.h"
 
 namespace Util
 {
 
 	template <class T>
-	concept IMessageDerived = std::is_base_of_v<client::IMessage, T>;
+	concept IMessageDerived = std::is_base_of_v<IMessage, T>;
 
 	constexpr uint32_t line(std::source_location source = std::source_location::current())
 	{
