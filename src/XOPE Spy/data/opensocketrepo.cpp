9053fbabf3e8975acbd177495860bc2c2ba58517
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2023-03-13 22:25:44 +0000

    Created a separate class containing info about opened sockets

---
+++ src/XOPE Spy/data/opensocketrepo.cpp
@@ -0,0 +1,137 @@
+#include "opensocketrepo.h"
+
+void OpenSocketsRepo::add(SOCKET socket)
+{
+	_socketsData.tryAdd(socket, {});
+	_recvPacketsToInject.tryAdd(socket, {});
+}
+
+void OpenSocketsRepo::remove(SOCKET socket)
+{
+	_socketsData.tryRemove(socket);
+	_recvPacketsToInject.tryRemove(socket);
+}
+
+bool OpenSocketsRepo::contains(SOCKET socket)
+{
+	return _socketsData.contains(socket);
+}
+
+void OpenSocketsRepo::markSocketAsTunnelled(SOCKET socket)
+{
+	_socketsData.tryUpdateValueFunc(socket, [](SocketData& sd)
+		{
+			sd.isTunneled = true;
+			sd.socketIdSentToSink = false;
+		});
+}
+
+void OpenSocketsRepo::stopTunnelingSocket(SOCKET socket)
+{
+	_socketsData.tryUpdateValueFunc(socket, [](SocketData& sd)
+		{
+			sd.isTunneled = false;
+		});
+}
+
+bool OpenSocketsRepo::isSocketTunneled(SOCKET socket)
+{
+	return _socketsData.contains(socket) && _socketsData[socket].isTunneled;
+}
+
+bool OpenSocketsRepo::wasSocketIdSentToSink(SOCKET socket)
+{
+	return _socketsData[socket].socketIdSentToSink;
+}
+
+void OpenSocketsRepo::emitSocketIdSentToSink(SOCKET socket)
+{
+	_socketsData.tryUpdateValueFunc(socket, [](SocketData& sd)
+		{
+			sd.socketIdSentToSink = true;
+		});
+}
+
+std::optional<bool> OpenSocketsRepo::isSocketNonBlocking(SOCKET socket)
+{
+	return _socketsData[socket].isBlocking;
+}
+
+void OpenSocketsRepo::setSocketToNonBlocking(SOCKET socket)
+{
+	_socketsData.tryUpdateValueFunc(socket, 
+		[](SocketData& sd)
+		{
+			sd.socketIdSentToSink = true;
+		});
+}
+
+void OpenSocketsRepo::setSocketIpVersion(SOCKET socket, int ipVersion)
+{
+	_socketsData.tryUpdateValueFunc(socket,
+		[=](SocketData& sd)
+		{
+			sd.ipVersion = ipVersion;
+		});
+}
+
+std::optional<int> OpenSocketsRepo::getSocketIpVersion(SOCKET socket)
+{
+	if (_socketsData.contains(socket))
+		return std::nullopt;
+
+	return _socketsData[socket].ipVersion;
+}
+
+
+void OpenSocketsRepo::addRecvPacketToInject(SOCKET socket, const Packet& packet)
+{
+	if (!_recvPacketsToInject.contains(socket))
+		_recvPacketsToInject.tryAdd(socket, std::queue<Packet>());
+
+	_recvPacketsToInject.tryUpdateValueFunc(socket,
+		[&](std::queue<Packet>& packets)
+		{
+			packets.push(packet);
+		});
+}
+
+std::optional<Packet> OpenSocketsRepo::getNextRecvPacketToInject(SOCKET socket)
+{
+	if (!_recvPacketsToInject.contains(socket) || recvPacketsToInjectCount(socket) < 1)
+		return std::nullopt;
+
+	std::optional<Packet> nextRecvPacket = std::nullopt;
+
+	_recvPacketsToInject.tryUpdateValueFunc(socket,
+		[&](std::queue<Packet>& packetQueue)
+		{
+			if (packetQueue.empty())
+				return;
+			
+			nextRecvPacket = std::move(packetQueue.front());
+			packetQueue.pop();
+		});
+
+	return nextRecvPacket;
+}
+
+size_t OpenSocketsRepo::recvPacketsToInjectCount(SOCKET socket)
+{
+	if (!_recvPacketsToInject.contains(socket))
+		return 0;
+
+	return _recvPacketsToInject[socket].size();
+}
+
+void OpenSocketsRepo::clearInjectableRecvPackets(SOCKET socket)
+{
+	if (!_recvPacketsToInject.contains(socket))
+		return;
+
+	_recvPacketsToInject.tryUpdateValueFunc(socket,
+		[=](std::queue<Packet>& packetQueue)
+		{
+			packetQueue = {};
+		});
+}
---
+++ src/XOPE Spy/data/opensocketrepo.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <mutex>
+#include <unordered_map>
+#include "../definitions/socketdata.h"
+#include "../utils/concurrent_unordered_map.h"
+
+/// <summary>
+/// Stores information about open sockets. Will auto-add socket if it does not exist but a flag is modified for that socket id
+/// </summary>
+class OpenSocketsRepo
+{
+public:
+	void add(SOCKET socket);
+	void remove(SOCKET socket);
+	bool contains(SOCKET socket);
+
+	void markSocketAsTunnelled(SOCKET socket);
+	void stopTunnelingSocket(SOCKET socket);
+	bool isSocketTunneled(SOCKET socket);
+
+	bool wasSocketIdSentToSink(SOCKET socket);
+	void emitSocketIdSentToSink(SOCKET socket);
+
+	//returns std::nullopt if socket's 'connect' call was not intercepted
+	std::optional<bool> isSocketNonBlocking(SOCKET socket);
+	void setSocketToNonBlocking(SOCKET socket);
+
+	std::optional<int> getSocketIpVersion(SOCKET socket);
+	void setSocketIpVersion(SOCKET socket, int ipVersion);
+
+	std::optional<Packet> getNextRecvPacketToInject(SOCKET socket);
+	void addRecvPacketToInject(SOCKET socket, const Packet& packet);
+
+	size_t recvPacketsToInjectCount(SOCKET socket);
+	void clearInjectableRecvPackets(SOCKET socket);
+
+private:
+	ConcurrentUnorderedMap<SOCKET, SocketData> _socketsData;
+	ConcurrentUnorderedMap<SOCKET, std::queue<Packet>> _recvPacketsToInject; // recv & WSARecv
+};
